07393774abb8f1d5243b4f84bda3596a82b55bfa
Luka-Magic <74438554+Luka-Magic@users.noreply.github.com>
2022-11-13 06:10:41 +0900

    update

--- exp/exp_0003/main.py
+++ exp/exp_0003/main.py
@@ -15,8 +15,8 @@ import sys
 @hydra.main(config_path='config', config_name='config')
 def main(cfg: DictConfig):
     # 環境をインポート
-    print(os.getcwd())
-    sys.path.append(f'../../env/{cfg.env_name}')
+    print(os.listdir('../../../../../'))
+    sys.path.append(f'../../../../../env/{cfg.env_name}')
     import gym_2048
 
     # 設定
