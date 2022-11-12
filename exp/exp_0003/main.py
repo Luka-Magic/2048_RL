98094d41b8c9465f76187ace76d5829bfc3a98c8
Luka-Magic <74438554+Luka-Magic@users.noreply.github.com>
2022-11-13 06:06:11 +0900

    u

--- exp/exp_0003/main.py
+++ exp/exp_0003/main.py
@@ -11,8 +11,8 @@ from env_wrapper import env_wrappers
 from agent import Agent
 import warnings
 import sys
-sys.path.append('../env/env0001')
-print(os.listdir('../'))
+sys.path.append('../../env/env0001')
+print(os.listdir('../../'))
 import gym_2048
 print(os.getcwd())
 
